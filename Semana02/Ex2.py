40da4692590921075075cd5d0dda2f387e12ae52
Vinicius Müller <viniciusmuller526@gmail.com>
2026-08-13 21:52:03 -0300

    Update Ex2.py

--- Semana02/Ex2.py
+++ Semana02/Ex2.py
@@ -38,24 +38,34 @@ class ContaBancaria:
 
                 print("Saldo insuficiente")
 
-    def trasferir_valor(self, quantidade_a_trasferir):
+    def trasferir_valor(self, contas_banco, conta_destino, quantidade_a_trasferir):
+
     
-            if quantidade_a_trasferir > self.saldo:
+        if quantidade_a_trasferir > self.saldo:
     
-                print("Saldo insuficiente")
+            print("Saldo insuficiente")
     
+        else:
+
+            if quantidade_a_trasferir > 0:
+
+                for contas in contas_banco:
+                    
+                    if conta_destino == contas.numero_conta:
+
+                        contas.saldo += quantidade_a_trasferir
+                        self.saldo -= quantidade_a_trasferir
+                        return
+
+                print("Conta n encontrada")
+
             else:
 
-                if quantidade_a_trasferir > 0:
-
-                    self.saldo -= quantidade_a_trasferir
-
-                else:
-
-                    print("Valor inválido")
+                print("Valor inválido")
 
 conta_vinicius = ContaBancaria("Vinícius", 1414, 3000)
 conta_pedro = ContaBancaria("Pedro", 1313, 1000)
+contas_banco = [conta_vinicius, conta_pedro]
 
 conta_vinicius.mostrar_conta()
 conta_vinicius.realizar_saque(1000)
@@ -65,5 +75,6 @@ conta_vinicius.mostrar_conta()
 
 conta_pedro.realizar_saque(1500)
 conta_pedro.realizar_deposito(0)
-conta_pedro.trasferir_valor(100)
+conta_pedro.trasferir_valor(contas_banco, 1414, 100)
 conta_pedro.mostrar_conta()
+conta_vinicius.mostrar_conta()
